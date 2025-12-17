76ef497aaf9f5c4fdea7fae8d67e3da3d5c20e9b
shelser <146368202+shelser@users.noreply.github.com>
2025-12-17 15:25:49 +0000

    added new files

--- src/const.js
+++ src/const.js
@@ -1,4 +1,11 @@
-export const API_URL = 'http://localhost:8024';
+const SERVER_URL = 'https://expert-fishstick-r4gq6vg4xjw9cvvg-8024.app.github.dev';
+const LOCAL_URL = 'http://localhost:8024';
+
+export const API_URL = window.location.hostname.includes('github.dev')
+  ? SERVER_URL
+  : LOCAL_URL;
+
+
 export const GOODS_URL = `${API_URL}/api/goods`;
 export const CATEGORY_URL = `${API_URL}/api/categories`;
 export const COLORS_URL = `${API_URL}/api/colors`;
